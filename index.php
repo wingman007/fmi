0c257d98c0357849638e07a5f946310bd88467ca
Edguybg <aleksaander_aleksandrov@abv.bg>
2013-01-18 12:21:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
      <title>Hello World</title>
   </head>
   <body>
-     <?php echo '<p>Hello World</p>'; ?>
+     <?php echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
   </body>
 </html>
