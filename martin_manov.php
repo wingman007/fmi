5a80557f585843924c8326e2ffbee8e00003d1c4
matrixatp.martin@gmail.com <cheresharov@hotmail.com>
2013-01-18 17:02:30 +0000

    асддфдсфгсдфг

--- martin_manov.php
+++ martin_manov.php
@@ -3,6 +3,12 @@
      <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Martin Manov - matrixatp.martin@gmail.com</p>'; ?>
+    <?php echo '<p>Martin Manov - matrixatp.martin@gmail.com</p>
+        Здравейте, 
+            имах малко проблеми с изпращането до GitHub.
+            Първо трябваше да отида до Git->Merge. След това Commit продължаваше да не работи, но успешно направих Push.
+            Дано това да може да помогне на останалите, ако има затруднения.
+        Поздрави
+    '; ?>
   </body>
 </html>
