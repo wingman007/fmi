03709b89d1647f95e05418ed18874959d76fdc9d
todorvelichkov89@gmail.com <>
2013-01-19 15:37:44 +0000

    ъбдейт на todor.php

--- Todor.php
+++ Todor.php
@@ -4,5 +4,7 @@
   </head>
   <body>
      <?php echo '<p>Hello from Todor</p>'; ?>
+    Този проект може да бъде разгледан на адрес: fmi-project-todor.eu01.aws.af.cm
+    Публично резозитори(форк): git@github.com:asdator/fmi.git
   </body>
 </html>
