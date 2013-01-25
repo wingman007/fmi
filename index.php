e78f86130c0dfe483c127466fdc756e035e090dd
elenachavdarova91@gmail.com <cheresharov@hotmail.com>
2013-01-25 09:53:42 +0000

    second

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
   </head>
   <body>
 <<<<<<< HEAD
+<<<<<<< HEAD
      <?php echo '<p>Hello World</p>'; ?>
 =======
      <?php echo '<p>Hello World from FMI</p>'; ?>
     <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
     <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
+    <?php // echo '<p>Hello World, Elena Chavdarova</p> <br> email:elenachavdarova91@gmail.com'; ?>
+    
 >>>>>>> refs/heads/master
+=======
+     <?php echo '<p>Hello World from FMI</p>'; ?>
+    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
+    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
+
+>>>>>>> refs/remotes/git hub remote/master
   </body>
 </html>
