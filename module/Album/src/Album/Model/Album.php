6cc964490e1de08dd5092b1759e2ede5989ed290
OpenShift guest <zdrachnik@abv.bg>
2013-02-08 04:42:46 -0500

    doesnt work

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -1,6 +1,11 @@
 <?php
 namespace Album\Model;
 
+use Zend\InputFilter\Factory as InputFactory;     // <-- Add this import
+use Zend\InputFilter\InputFilter;                 // <-- Add this import
+use Zend\InputFilter\InputFilterAwareInterface;   // <-- Add this import
+use Zend\InputFilter\InputFilterInterface;        // <-- Add this import
+
 class Album implements InputFilterAwareInterface
 {
     public $id;
