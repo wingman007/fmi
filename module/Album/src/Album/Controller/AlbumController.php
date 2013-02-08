fdf2e58b64656e3a3bf753a7d1e150b9ac5853c4
cheresharov@hotmail.com <cheresharov@hotmail.com>
2013-02-08 08:52:46 +0000

    test

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -21,24 +21,6 @@ class AlbumController extends AbstractActionController
     // Add content to this method:
     public function addAction()
     {
-        $form = new AlbumForm();
-        $form->get('submit')->setValue('Add');
-
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            $album = new Album();
-            $form->setInputFilter($album->getInputFilter());
-            $form->setData($request->getPost());
-
-            if ($form->isValid()) {
-                $album->exchangeArray($form->getData());
-                $this->getAlbumTable()->saveAlbum($album);
-
-                // Redirect to list of albums
-                return $this->redirect()->toRoute('album');
-            }
-        }
-        return array('form' => $form);
     }
 
     public function editAction()
