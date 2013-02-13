5f3b27ebe59dfc9219d560b5d71e00619f90364f
stoyancheresharov@gmail.com <>
2013-02-13 13:29:12 +0000

    I have changed the layout

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -82,7 +82,8 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+          //            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/student.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
           //            'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
