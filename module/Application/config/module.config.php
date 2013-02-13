8725973aacc90ad138f4b1dd75005ba576fb17b2
stoyancheresharov@gmail.com <>
2013-02-13 11:42:08 +0000

    commented the view script to see if it works

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -82,7 +82,7 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
+          //            'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
