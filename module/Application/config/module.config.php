21270619286a70eae6a6c7d9de8e716fa3d4d272
stoyancheresharov@gmail.com <>
2013-02-13 11:18:25 +0000

    Correcting the record in module.config.php

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -69,8 +69,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
-          //            'Application\Controller\Stoyan' => 'Application\Controller\StoyanController' // <-- Added by me
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Stoyan' => 'Application\Controller\StoyanController' // <-- Added by me
         ),
     ),
     'view_manager' => array(
@@ -82,7 +82,7 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-          //          'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
+            'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
