6e99793053079214a0a73cc73371eb45e137e043
Zh G <perkoz@mail.bg>
2013-02-15 21:23:40 +0000

    wrong namespace fixed

--- module/ZhelyanGuglev/src/ZhelyanGuglev/Controller/IndexController.php
+++ module/ZhelyanGuglev/src/ZhelyanGuglev/Controller/IndexController.php
@@ -7,7 +7,7 @@
 * @license http://framework.zend.com/license/new-bsd New BSD License
 */
 
-namespace StoyanCheresharov\Controller;
+namespace ZhelyanGuglev\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
