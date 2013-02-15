9681bfb65224517d3167aaba70d3e1f9aa7bde93
npoibrenska91@gmail.com <>
2013-02-15 16:43:32 +0000

    rr

--- index.php
+++ index.php
@@ -1,37 +1,6 @@
 <html>
   <head>
-     <title>Hello World</title>
+     <title>Hello World by Nikoleta</title>
   </head>
   <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <?php echo '<p>Hello World</p>'; ?>
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-    <?php // echo '<p>Hello World, Elena Chavdarova</p> <br> email:elenachavdarova91@gmail.com'; ?>
     
->>>>>>> refs/heads/master
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
->>>>>>> refs/remotes/git hub remote/master
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
->>>>>>> refs/heads/master
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
->>>>>>> refs/heads/master
-  </body>
-</html>
