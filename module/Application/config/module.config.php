fd2cd677cd07169358e82699800d7a1894ea4dd9
OpenShift guest <aleksaander_aleksandrov@abv.bg>
2013-02-15 03:41:14 -0500

    Added Alexander

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -69,7 +69,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\AlexanderAlexandrov' => 'Application\Controller\AlexanderAlexandrovController'
         ),
     ),
     'view_manager' => array(
