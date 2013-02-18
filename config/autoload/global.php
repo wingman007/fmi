f64a968571f1bea0e722b5560913c37b8044b14b
wanya.dimitrowa@gmail.com <>
2013-02-18 13:24:18 +0000

    Add controllers

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,3 +14,19 @@
 return array(
     // ...
 );
+<?php
+return array(
+    'db' => array(
+        'driver'         => 'Pdo',
+
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+);
