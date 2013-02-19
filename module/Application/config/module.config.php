2d95f66e432fd6181516707ef012f73229257de2
wanya.dimitrowa@gmail.com <>
2013-02-19 20:12:41 +0000

    Change files

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -69,7 +69,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+             'Application\Controller\VanyaDimitrova' => 'Application\Controller\VanyaDimitrovaController'
         ),
     ),
     'view_manager' => array(
