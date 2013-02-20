6ca85c8c311e39df8544d9fcb90524deafc65527
Zh G <perkoz@mail.bg>
2013-02-20 15:20:28 +0000

    wrong module path

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -12,7 +12,7 @@ return array(
             'album' => array(
                 'type'    => 'segment',
                 'options' => array(
-                    'route'    => '/album[/:action][/:id]',
+                  'route'    => '/album/album[/:action][/:id]',
                     'constraints' => array(
                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                         'id'     => '[0-9]+',
