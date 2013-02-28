b09f8a0dc5cadd623970bc438ac94f0ad00fe202
elyned@abv.bg <>
2013-02-28 22:38:47 +0000

    af

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -27,11 +27,3 @@ return array(
         ),
     ),
 );
- * You can use this file for overridding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
