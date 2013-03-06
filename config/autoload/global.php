3a1d04fcf53fd3f8062d4bf63500e6bb157c837b
Stoyan Cheresharov <cheresharov@hotmail.com>
2013-03-06 13:12:27 +0200

    Merging the differences with Viktor

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,20 +1,26 @@
 <?php
 /**
-<<<<<<< HEAD
- * Global Configuration Override
- *
- * You can use this file for overridding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
+* Global Configuration Override
+*
+* You can use this file for overridding configuration values from modules, etc.
+* You would place values in here that are agnostic to the environment and not
+* sensitive to security.
+*
+* @NOTE: In practice, this file will typically be INCLUDED in your source
+* control, so do not include passwords or other sensitive information in this
+* file.
+*/
+
+/*
+return array(
+// ...
+);
+*/
 
 return array(
     'db' => array(
         'driver' => 'Pdo',
+// 'dsn' => 'mysql:dbname=wingman;host=wingman-db.my.phpcloud.com',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
@@ -26,4 +32,3 @@ return array(
         ),
     ),
 );
-
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
+* Zend Framework (http://framework.zend.com/)
+*
+* @link http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+* @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+* @license http://framework.zend.com/license/new-bsd New BSD License
+*/
 
 return array(
     'router' => array(
@@ -13,15 +13,11 @@ return array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route'    => '/',
+                    'route' => '/',
                     'defaults' => array(
-<<<<<<< HEAD
-                        'controller' => 'Album\Controller\Album', // <-- change here
-=======
-//                      'controller' => 'Album\Controller\Album',
+// 'controller' => 'Album\Controller\Album',
                         'controller' => 'Application\Controller\Index',
->>>>>>> e6069389b80e7d3f1e0b75bd0fd59d987ac36951
-                        'action'     => 'index',
+                        'action' => 'index',
                     ),
                 ),
             ),
@@ -30,24 +26,24 @@ return array(
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
             'application' => array(
-                'type'    => 'Literal',
+                'type' => 'Literal',
                 'options' => array(
-                    'route'    => '/application',
+                    'route' => '/application',
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
+                        'controller' => 'Index',
+                        'action' => 'index',
                     ),
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
                     'default' => array(
-                        'type'    => 'Segment',
+                        'type' => 'Segment',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
+                            'route' => '/[:controller[/:action]]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
                             ),
                             'defaults' => array(
                             ),
@@ -66,43 +62,35 @@ return array(
         'locale' => 'en_US',
         'translation_file_patterns' => array(
             array(
-                'type'     => 'gettext',
+                'type' => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
+                'pattern' => '%s.mo',
             ),
         ),
     ),
     'controllers' => array(
         'invokables' => array(
-<<<<<<< HEAD
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
-=======
             'Application\Controller\Index' => 'Application\Controller\IndexController',
-//       'Application\Controller\ZhelyanGuglev' => 'Application\Controller\ZhelyanGuglevController',
-//          'Application\Controller\AlexanderAlexandrov' => 'Application\Controller\AlexanderAlexandrovController'
-//          'Application\Controller\Stoyan' => 'Application\Controller\StoyanController', // <-- Added by me
-//          'Application\Controller\StoyanCheresharov' => 'Application\Controller\StoyanCheresharovController', // <-- Added by me
-//          'Application\Controller\FmiStudent' => 'Application\Controller\FmiStudentController', // <-- Added by me
->>>>>>> e6069389b80e7d3f1e0b75bd0fd59d987ac36951
+// 'Application\Controller\ZhelyanGuglev' => 'Application\Controller\ZhelyanGuglevController',
+// 'Application\Controller\AlexanderAlexandrov' => 'Application\Controller\AlexanderAlexandrovController'
+// 'Application\Controller\Stoyan' => 'Application\Controller\StoyanController', // <-- Added by me
+// 'Application\Controller\StoyanCheresharov' => 'Application\Controller\StoyanCheresharovController', // <-- Added by me
+// 'Application\Controller\FmiStudent' => 'Application\Controller\FmiStudentController', // <-- Added by me
         ),
     ),
     'view_manager' => array(
         'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-<<<<<<< HEAD
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+      'layout/student' => __DIR__ . '/../view/layout/student.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-=======
-      'layout/student'           => __DIR__ . '/../view/layout/student.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-//          'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
->>>>>>> e6069389b80e7d3f1e0b75bd0fd59d987ac36951
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+// 'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
