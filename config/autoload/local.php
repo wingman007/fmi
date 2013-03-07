d03d916e76cd6830745861c186f06a4fd4116004
Zh G <perkoz@mail.bg>
2013-03-07 11:03:31 +0200

    merge

--- config/autoload/local.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'db' => array(
-        'dsn'            => 'mysql:dbname=fmipu;host=fmipu-db.my.phpcloud.com',        
-        'username' => 'fmipu',
-        'password' => 'upimfx2',
-    ),
-);
