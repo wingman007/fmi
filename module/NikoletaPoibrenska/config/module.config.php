3345e260476619140d374fd225a9981746213770
npoibrenska91@gmail.com <>
2013-03-09 14:23:43 +0000

    module nikoleta

---
+++ module/NikoletaPoibrenska/config/module.config.php
@@ -0,0 +1,42 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'NikoletaPoibrenska\Controller\Index' => 'NikoletaPoibrenska\Controller\IndexController',
+        ),
+    ),
+  
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'stoyan_cheresharov' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/nikoleta-poibrenska[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'NikoletaPoibrenska\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+  
+    'view_manager' => array(
+        'template_map' => array(
+          // 'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml', // the entire app is using it
+          //            'layout/layout'           => __DIR__ . '/../view/layout/student.phtml',
+          //  'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+          //  'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
+          //  'error/404'               => __DIR__ . '/../view/error/404.phtml',
+          //  'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            'nikoleta_poibrenska' => __DIR__ . '/../view',
+        ),
+    ),
+);
