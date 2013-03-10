1830789b9334945883b8e2f9b3ff8ce510e95ad8
cheresharov@yahoo.com <>
2013-03-10 09:59:46 +0000

    Added a layout

--- module/StoyanCheresharov/config/module.config.php
+++ module/StoyanCheresharov/config/module.config.php
@@ -29,7 +29,7 @@ return array(
     'view_manager' => array(
         'template_map' => array(
           // 'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml', // the entire app is using it
-          // 'layout/StoyanCheresharov'           => __DIR__ . '/../view/layout/StoyanCheresharov.phtml', // the entire app is using it
+          'layout/StoyanCheresharov'           => __DIR__ . '/../view/layout/StoyanCheresharov.phtml', // the entire app is using it
           //            'layout/layout'           => __DIR__ . '/../view/layout/student.phtml',
           //  'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
           //  'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
