3e970e4e56fae0e71723b7795b7c3a274d5356f0
Zh G <perkoz@mail.bg>
2013-03-11 07:59:55 +0000

    wrong layout path configuration fixed

--- module/ZhelyanGuglev/config/module.config.php
+++ module/ZhelyanGuglev/config/module.config.php
@@ -30,7 +30,7 @@ return array(
         'template_map' => array(
           // 'layout/layout' => __DIR__ . '/../view/layout/layout.phtml', // the entire app is using it
           'layout/ZhelyanGuglev' => __DIR__ . '/../view/layout/ZhelyanGuglev.phtml',
-          'layout/zheponymous' =>  __DIR__ . '/../view/layout/eponymous.phtml',
+          'layout/zheponymous' =>  __DIR__ . '/../view/layout/eponymous/eponymous.phtml',
           // 'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
           // 'application/stoyan/index' => __DIR__ . '/../view/application/stoyan/index.phtml', // <-- Added by me
           // 'error/404' => __DIR__ . '/../view/error/404.phtml',
