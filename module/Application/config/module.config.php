0a10fa554df5a4d4e2481beda9faa3eb63a77030
wanya.dimitrowa@gmail.com <>
2013-03-11 22:53:27 +0000

    Delete some files

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -80,7 +80,7 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/index1.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
