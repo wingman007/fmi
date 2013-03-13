61c491d8677b2ce5a1bcb6a20b9d0bff042b4c7c
Stoyan Cheresharov <cheresharov@hotmail.com>
2013-03-13 14:53:19 +0200

    merging KrystioPetrov

--- index.php
+++ index.php
@@ -3,50 +3,9 @@
      <title>Hello World</title>
   </head>
   <body>
-<<<<<<< HEAD
-     <?php echo '<p>Hello World</p>'; ?>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <?php echo '<p>Hello World</p>'; ?>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <?php echo '<p>Hello World</p>'; ?>
-=======
      <?php echo '<p>Hello World from FMI</p>'; ?>
     <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
     <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
     <?php // echo '<p>Hello World, Elena Chavdarova</p> <br> email:elenachavdarova91@gmail.com'; ?>
-    
->>>>>>> refs/heads/master
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
->>>>>>> refs/remotes/git hub remote/master
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
->>>>>>> refs/heads/master
-=======
-     <?php echo '<p>Hello World from FMI</p>'; ?>
-    <?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-
->>>>>>> refs/heads/master
->>>>>>> refs/remotes/fmi@github/master
-=======
-    <?php echo '<p>Hello World from FMI</p>'; ?>
-	<?php // echo '<p>Hello World from Mihail Kopuschu! meshko1989@mail.ru</p>'; ?>
-    <?php // echo '<p>Hello World, Aleksander Aleksandrov</p> <br> email:aleksaander_aleksandrov@abv.bg'; ?>
-    <?php // echo '<p>Hello World, Elena Chavdarova</p> <br> email:elenachavdarova91@gmail.com'; ?>
->>>>>>> e6069389b80e7d3f1e0b75bd0fd59d987ac36951
->>>>>>> refs/heads/master
   </body>
 </html>
