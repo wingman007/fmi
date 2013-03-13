6b0469ddcf5a8839ea2ded433c44a7f768046dd7
elyned@abv.bg <>
2013-03-13 13:33:56 +0000

    Changing the config.

--- config/application.config.php
+++ config/application.config.php
@@ -9,6 +9,7 @@ return array(
         'StoyanCheresharov',                  // <-- Add this line
         'FmiStudent',                  // <-- Add this line
         'StudentBg',
+        'ElitsaNedyalkova',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
