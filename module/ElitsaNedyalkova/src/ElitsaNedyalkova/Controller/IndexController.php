ce6ec5b31f8742d13ade428101de3ef908a40714
elyned@abv.bg <>
2013-03-13 17:32:52 +0000

    After creating a new layout some changes had been made.

--- module/ElitsaNedyalkova/src/ElitsaNedyalkova/Controller/IndexController.php
+++ module/ElitsaNedyalkova/src/ElitsaNedyalkova/Controller/IndexController.php
@@ -21,7 +21,6 @@ class IndexController extends AbstractActionController
   
   public function changeAction()
   {
-    $id = $this->getRequest()->getParam('id');
      $this->layout('layout/ElitsaNedyalkova');
     return new ViewModel();
   }
