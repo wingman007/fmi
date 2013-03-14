8d7559c7f9f06dbd7536274842736fd09381e851
ness.leehan@gmail.com <>
2013-03-14 13:16:28 +0000

    After merging 3

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -91,12 +91,6 @@ return array(
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/index' => __DIR__ . '/../view/error/index.phtml',
 
- * Zend Framework (http://framework.zend.com/)
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 return array(
     'router' => array(
         'routes' => array(
