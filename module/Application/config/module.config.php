50ce022f0272fe8f900b46a3e3420cdeeb4723ea
ness.leehan@gmail.com <>
2013-03-14 13:11:41 +0000

    After merging

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,7 +1,6 @@
 <?php
 /**
 * Zend Framework (http://framework.zend.com/)
-*
 * @link http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
 * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
 * @license http://framework.zend.com/license/new-bsd New BSD License
@@ -93,7 +92,6 @@ return array(
             'error/index' => __DIR__ . '/../view/error/index.phtml',
 
  * Zend Framework (http://framework.zend.com/)
- *
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
