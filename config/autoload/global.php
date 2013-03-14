f05a38387b5a6edbdec36370e7345509a9e31f3a
rciripulov@gmail.com <>
2013-03-14 21:28:11 +0000

    Fix error

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,40 +1,5 @@
 <?php
 /**
-
-* Global Configuration Override
-*
-* You can use this file for overridding configuration values from modules, etc.
-* You would place values in here that are agnostic to the environment and not
-* sensitive to security.
-*
-* @NOTE: In practice, this file will typically be INCLUDED in your source
-* control, so do not include passwords or other sensitive information in this
-* file.
-*/
-
-/*
-return array(
-// ...
-);
-*/
-
-return array(
-    'db' => array(
-        'driver' => 'Pdo',
-// 'dsn' => 'mysql:dbname=wingman;host=wingman-db.my.phpcloud.com',
-        'driver_options' => array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
-    ),
-    'service_manager' => array(
-        'factories' => array(
-            'Zend\Db\Adapter\Adapter'
-                    => 'Zend\Db\Adapter\AdapterServiceFactory',
-        ),
-    ),
-);
-
-
  * Global Configuration Override
  *
  * You can use this file for overridding configuration values from modules, etc.
@@ -47,5 +12,18 @@ return array(
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver'         => 'Pdo',
+
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
+
