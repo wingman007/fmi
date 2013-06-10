e8db12166f7a732806a2989ceb278e48137c5004
vladant@abv.bg <>
2013-06-10 10:38:41 +0000

    Vladislav created

---
+++ vendor/ZendFramework/library/Zend/Http/Header/HeaderInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Http\Header;
+
+interface HeaderInterface
+{
+    public static function fromString($headerLine);
+    public function getFieldName();
+    public function getFieldValue();
+    public function toString();
+}
